ae9ea2d8f12372ee81e6aa6db08a3e5201ac0f4e
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-03-09 23:59:14 +0900

    add firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCxuutlUCSafJ552tcgSi1hrDQLQYL-u_I",
+  authDomain: "iot-rice-coocker.firebaseapp.com",
+  databaseURL: "https://iot-rice-coocker.firebaseio.com",
+  projectId: "iot-rice-coocker",
+  storageBucket: "iot-rice-coocker.appspot.com",
+  messagingSenderId: "1033400838589"
+};
