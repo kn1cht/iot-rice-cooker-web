a8d7c57e0cf7a8f04d1748df1064235f5bbdf03f
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-03-25 12:39:24 +0900

    add font-awesome icons

--- src/components/CookerSummaryCard.js
+++ src/components/CookerSummaryCard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { keyframes } from "styled-components";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,16 +11,33 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+library.add(
+  faCog
+)
+
 const styles = {
   card: {
     maxWidth: 275,
     marginButtom: 10,
     marginTop: 10,
   },
+  leftIcon: {
+    marginRight: 10,
+  },
   title: {
     fontSize: 14,
   },
 };
+
+const spin = keyframes`
+0% { transform: rotate(0deg); }
+100% { transform: rotate(360deg); }
+`;
+
+const Spinner = styled.div`
+animation: ${spin} infinite 5s linear;
+display: inline-block;
+`;
 
 const CookerSummaryCard = props => {
   const { classes, cooker } = props;
@@ -28,6 +49,9 @@ const CookerSummaryCard = props => {
           概要
         </Typography>
         <Typography variant="h4" component="h2" gutterBottom>
+          <Spinner className={classes.leftIcon}>
+            <FontAwesomeIcon icon={faCog} size="lg" />
+          </Spinner>
           {cooker.active ? '炊飯中' : '待機中'}
         </Typography>
         <Typography variant="h5" gutterBottom>
--- src/components/CookerSupplyCard.js
+++ src/components/CookerSupplyCard.js
@@ -1,9 +1,18 @@
+import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
+library.add(
+  faCheckCircle,
+  faTimesCircle
+)
 
 const styles = {
   card: {
@@ -14,10 +23,29 @@ const styles = {
   title: {
     fontSize: 14,
   },
+  leftIcon: {
+    marginRight: 4,
+  },
+  greenIcon: {
+    color: 'limegreen',
+  },
+  redIcon: {
+    color: 'red',
+  },
 };
 
 const CookerSupplyCard = props => {
   const { classes, cooker } = props;
+
+  const supplyIcon = ok => {
+    return ok ? (
+      <FontAwesomeIcon className={classNames(classes.leftIcon, classes.greenIcon)}
+      icon={faCheckCircle} size="lg" />
+    ) : (
+      <FontAwesomeIcon className={classNames(classes.leftIcon, classes.redIcon)}
+      icon={faTimesCircle} size="lg" />
+    );
+  };
 
   return (
     <Card className={classes.card}>
@@ -26,15 +54,17 @@ const CookerSupplyCard = props => {
           消耗品
         </Typography>
         <Typography variant="h5">
-          水タンク
+          給水タンク
         </Typography>
         <Typography component="p" gutterBottom>
+          {supplyIcon(cooker.water)}
           {cooker.water ? '残量あり' : '残量不足'}
         </Typography>
         <Typography variant="h5">
           廃水タンク
         </Typography>
         <Typography component="p">
+          {supplyIcon(!cooker.waste)}
           {cooker.waste ? '満水' : '余裕あり'}
         </Typography>
       </CardContent>
